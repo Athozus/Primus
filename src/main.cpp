0b469c30e8eb0a8629518a8b09ce9f29efe898b8
AirSThib <thibauld.scellet@gmail.com>
2021-04-11 19:37:26 +0200

    Fixing font problem

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-    a.setApplicationDisplayName("Primus v.0.2.1 αlpha");
+	a.setApplicationDisplayName("Primus v.0.2.1 αlpha");
 
-    QFile file(a.applicationDirPath() + "/style.qss");
+	QFontDatabase::addApplicationFont("RedHatDisplay-Regular.ttf");
+        QFont redHatDisplay("RedHatDisplay-Regular", 16, QFont::Normal);
+        a.setFont(redHatDisplay);
+
+    	QFile file(a.applicationDirPath() + "/style.qss");
 	file.open(QFile::ReadOnly);
 	QString styleSheet = QLatin1String(file.readAll());
 	a.setStyleSheet(styleSheet);
-
-	QFontDatabase::addApplicationFont("RedHatDisplay-Regular.ttf");
-	QFont redHatDisplay("RedHatDisplay-Regular", 16, QFont::Normal);
-	a.setFont(redHatDisplay);
     
 	MainWindow w;
 	w.show();
